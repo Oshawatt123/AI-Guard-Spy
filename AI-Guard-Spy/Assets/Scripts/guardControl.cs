7685a22d8e2b3eed749a009cdd042588f6d38ab7
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2020-02-07 12:04:48 +0000

    Objects move around and see each other through walls

---
+++ AI-Guard-Spy/Assets/Scripts/guardControl.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class guardControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform[] path;
+    NavMeshAgent agent;
+    private int destPoint = 0;
+
+    private enum State
+    {
+        Patrolling,
+        Chasing
+    }
+
+    private State currentState = State.Patrolling;
+
+    private GameObject spy;
+
+    public float FOV;
+    public float viewDistance;
+    void Start()
+    {
+        spy = GameObject.FindGameObjectWithTag("Spy");
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+
+        GoToNextPoint();
+    }
+
+    void GoToNextPoint()
+    {
+        if(path.Length == 0)
+        {
+            return;
+        }
+
+        agent.destination = path[destPoint].position;
+
+        // loop through the points
+        destPoint = (destPoint + 1) % path.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentState == State.Patrolling)
+        {
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                Debug.Log("Agent Met goal");
+                GoToNextPoint();
+            }
+
+            if (Vector3.Distance(transform.position, spy.transform.position) < viewDistance && Vector3.Angle(transform.position, spy.transform.position) < FOV)
+            {
+                Debug.Log("Spy sighted!");
+                currentState = State.Chasing;
+            }
+        }
+        else if (currentState == State.Chasing)
+        {
+            agent.destination = spy.transform.position;
+        }
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        float y;
+        y = viewDistance / Mathf.Tan(((180 - FOV) / 2)* Mathf.PI/180);
+        Vector3 drawPoint = new Vector3(transform.position.x + y, transform.position.y, transform.position.z + viewDistance);
+        Gizmos.DrawLine(transform.position, drawPoint);
+        Vector3 drawPoint2 = new Vector3(transform.position.x - y, transform.position.y, transform.position.z + viewDistance);
+        Gizmos.DrawLine(transform.position, drawPoint2);
+    }
+}
---
+++ AI-Guard-Spy/Assets/Scripts/spyControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class spyControl : MonoBehaviour
+{
+    public Transform target;
+    NavMeshAgent agent;
+
+    public float FOV;
+    public float viewDistance;
+
+    public GameObject guard;
+
+    private bool spotted = false;
+
+    public Transform hidingSpot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!spotted)
+        {
+            Debug.Log("Distance to guard: " + Vector3.Distance(transform.position, guard.transform.position));
+            if (Vector3.Distance(transform.position, guard.transform.position) < viewDistance && Vector3.Angle(transform.position, guard.transform.position) < FOV)
+            {
+                Debug.Log("Spy sighted!");
+                spotted = true;
+            }
+        }
+        else
+        {
+            agent.destination = hidingSpot.position;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        float y;
+        y = viewDistance / Mathf.Tan(((180 - FOV) / 2) * Mathf.PI / 180);
+        Vector3 drawPoint = new Vector3(transform.position.x + y, transform.position.y, transform.position.z + viewDistance);
+        Gizmos.DrawLine(transform.position, drawPoint);
+        Vector3 drawPoint2 = new Vector3(transform.position.x - y, transform.position.y, transform.position.z + viewDistance);
+        Gizmos.DrawLine(transform.position, drawPoint2);
+    }
+}
